cac07e8d90309450656cb1aef0a328e1ba7cf8dc
Jonathanch2022 <admin@JCH-LPT2>
2026-04-09 16:53:20 -0400

    fixed header menu issue in contacts not rendering correct hearder menu

--- frontend/field-force-contractor/screens/Contacts.tsx
+++ frontend/field-force-contractor/screens/Contacts.tsx
@@ -5,7 +5,7 @@ import { SearchBar } from "../components/SearchBar"
 export const Contacts:FC = (props) => {
 
     return(<>
-    <MainFrame>
+    <MainFrame header="home" headerMenu={["Menu2",["Contacts"]]}>
      <SearchBar placeHolder="Search..." buttonText="Search"/>
 
     </MainFrame>
