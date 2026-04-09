569a770095e9c9369ba69c81c030452edb924085
Jonathanch2022 <admin@JCH-LPT2>
2026-04-08 21:41:26 -0400

    added view switch to menu

--- frontend/field-force-contractor/components/Menu.tsx
+++ frontend/field-force-contractor/components/Menu.tsx
@@ -1,8 +1,9 @@
 import {Styles} from "../constants/Styles"
-import {View, Text,Pressable} from "react-native"
+import {View, Text,Pressable,Image} from "react-native"
 import {FC} from "react"
-import {useState,useEffect} from "react"
+import {useState,useEffect,useRef} from "react"
 import { MenuItem } from "./MenuItem"
+
 type MenuVariant = "Menu1" | "Menu2" | "Menu3"
 export type MenuItems = {label:string,icon?:string,component:string}
 export type MenuOptions = [MenuVariant,items:MenuItems[]]
@@ -12,16 +13,19 @@ menuOptions?:MenuOptions
 export const Menu:FC<Props> = (props) => {
     const [options,setIptions] = useState(props.menuOptions)
     const [menuStyle,setMenuStyle] = useState<any>(null);
-    
+    const [viewItem, setView] = useState<FC>();
+  
     useEffect(()=>{
      
         setIptions(props.menuOptions);
         switch (options?.[0]){
         case "Menu1":
             setMenuStyle(Styles.Menu.container)
+            setView(v1)
             break;
         case "Menu2":
             setMenuStyle(Styles.Menu.container)
+            setView(v2)
             break;
 
         default:
@@ -31,27 +35,35 @@ export const Menu:FC<Props> = (props) => {
 
 
     },[options])
+    const v1 = () => {
+      return(
+     <View  style={Styles.Menu.container}>
+       {
+       options?.[1].map((items,index) =>{
+          return(<MenuItem key={index} menuItem={items}/>)
 
-    return(<>
-    {
-    (options)?  
-    <View style={menuStyle}>
+       })}
+      </View>)
+
+    }
+
+    const v2 = () =>{
+      return(
+        <View style={Styles.Menu.container}>
+          <Text style={Styles.TestStyles.Style1}>Testing View 2</Text>
+        </View>
+      )
+    }
+ 
+    return(
+      viewItem 
+    )
+
      
-      {
-        
-       options[1].map((items, index) =>{
-          console.log(items.label);
-        return(
-
-          <MenuItem key={index} menuItem={items}/>
-
-        )
-       })
-      }
-
-    </View> :null
+      
 }
-  </>)
+    
+  
+
    
 
-}
