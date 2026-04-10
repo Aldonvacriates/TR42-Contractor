60ae1675ce67748d3e8522d7bda2d4c504c6f5d3
Jonathanch2022 <admin@JCH-LPT2>
2026-04-10 16:54:45 -0400

    completed contacts card

--- frontend/field-force-contractor/components/ContactCard.tsx
+++ frontend/field-force-contractor/components/ContactCard.tsx
@@ -6,6 +6,7 @@ import {FC} from "react"
 type Props = {
 
     profileIcon?: string
+    name?:string
     phoneNumber?: number
     contactId?: number
 }
@@ -14,7 +15,41 @@ export const ContactCard:FC<Props> = (props) =>{
   return(<>
   
   <View style={Styles.Contacts.container}>
-   <Text style={Styles.Menu.itemText}>Test</Text>
+    
+    <View style={Styles.Contacts.IconContainer}>
+        <Image source={Assets.icons.ProfileIcon} style={Styles.Contacts.ProfileIcon}/>
+    </View>
+    <View style={Styles.Contacts.InfoContainer}>
+       <Text style={Styles.Contacts.contactText}>John Doe</Text>
+       <View style={Styles.Contacts.phoneIconText}>
+         <Image style={Styles.Contacts.phoneIcon} source={Assets.icons.PhoneIcon}/>
+         <Pressable>
+         {
+            ({pressed}) => {
+
+            return(<>
+            <Text style={(pressed) ? Styles.Contacts.contactText : Styles.Contacts.contactTextPressed}> 555-555-5555 </Text>
+            
+            </>)
+            }
+        }
+         </Pressable>
+        </View>
+        
+
+    </View>
+    <View style={Styles.Contacts.buttonContainer}>
+         <Pressable>
+            {
+                ({pressed}) => {
+                return(<>
+                    <Image source={Assets.icons.BackArrow} style={(pressed)?Styles.Contacts.forwardArrowPressed : Styles.Contacts.forwardArrow}/>
+               </>)
+                }
+            }           
+         </Pressable>
+    </View>
+
   </View>
   
   </>)
