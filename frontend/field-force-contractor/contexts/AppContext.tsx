ad38cb5edfe9be6ba61bd4bc5c31d02668dc1fc0
Jonathanch2022 <admin@JCH-LPT2>
2026-05-06 07:44:03 -0700

    Separate app branding and contact cleanup

--- frontend/field-force-contractor/contexts/AppContext.tsx
+++ frontend/field-force-contractor/contexts/AppContext.tsx
@@ -1,64 +1,78 @@
-import { createContext,useState,ReactNode } from "react"
-
-
+import { createContext, ReactNode, useState } from "react"
 
 export const  AppContext = createContext<any>(null)
-
 export type userTable = {
 
     userid:string,
     firstName:string,
     lastName:string,
     phone:string,
-    role:string
+    role:string,
+    vendorid:string
 }
 //Demo User Data
-export const demoUsers:userTable[] = [
+export const _DEV_MODE_DEMO_USERS:userTable[] = [
  {
     userid:"0",
     firstName:"John",
     lastName:"Doe",
     phone:"555-555-5555",
-    role:"contractor"
+    role:"contractor",
+    vendorid:"1"
  },
  {
     userid:"1",
     firstName:"Jane",
     lastName:"Doe",
     phone:"666-555-5555",
-    role:"client"
+    role:"client",
+    vendorid:""
  },
  {
     userid:"2",
     firstName:"Taylor",
     lastName:"Swith",
     phone:"777-555-5555",
-    role:"vendor"
+    role:"vendor",
+    vendorid:""
  },
  {
     userid:"3",
     firstName:"Ben",
     lastName:"Joe",
     phone:"888-555-5555",
-    role:"contractor"
+    role:"contractor",
+    vendorid:"1"
  }
 
 ]
 export const getUser = (userid:string) =>{
 
-    let user = demoUsers.find(p => p.userid === userid) || null
+    const user: userTable|boolean = _DEV_MODE_DEMO_USERS.find(p => p.userid === userid)  || false
+    if(user){
+      return(user)
+    }
+    return(false)
   
-    return(user);
 }
+export const _DEV_MODE_DEMO_CLIENT = (userid:string) => {
+     const user = getUser(userid);
+     if(user !== false ){
+     return{...user,lastName:`${user.lastName} [CLIENT]`}
+     }
+     return(user)
+   }
 export const AppProvider = ({children} : {children:ReactNode}) =>{
+
    const [mount,setMounted] = useState(false);
    const [reverseStack,setReverseStack] = useState(false);
    const [devMode,setDevMode] = useState(false);
-   const [userInfo,setUserInfo] = useState(getUser("1"));
+   const [userInfo,setUserInfo] = useState(getUser("0")); 
+   const [client,setClient] = useState<userTable | boolean>(_DEV_MODE_DEMO_CLIENT("1"));
    
     return(
 
-        <AppContext.Provider value={{mount,setMounted,reverseStack,setReverseStack,devMode,setDevMode,setUserInfo,userInfo}}>
+        <AppContext.Provider value={{mount,setMounted,reverseStack,setReverseStack,devMode,setDevMode,setUserInfo,userInfo,client,setClient}}>
         {children}
         </AppContext.Provider>
     )
--- frontend/field-force-contractor/screens/ContactScreen.tsx
+++ frontend/field-force-contractor/screens/ContactScreen.tsx
@@ -1,19 +1,23 @@
-import {FC,useState} from "react"
+import { RootStackParamList } from "@/App"
+import { ContactCard } from "@/components/ContactCard"
 import { MainFrame } from "@/components/MainFrame"
 import { SearchBar } from "@/components/SearchBar"
-import { ContactCard } from "@/components/ContactCard"
-import { useNavigation } from "@react-navigation/native"
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from "@/App"
-import { demoUsers } from "@/contexts/AppContext"
+import { AppContext, demoUsers } from "@/contexts/AppContext"
+import { useNavigation, useRoute } from "@react-navigation/native"
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { FC, useContext, useState } from "react"
 
 
 export const Contacts:FC = (props) => {
     
  
     const nav = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
-    const [contacts] = useState(demoUsers)
+    const {client} = useContext(AppContext)
+    const contacts = (client) ?  [...demoUsers,client] : demoUsers
     const  [nameSearch,setNameSearch] = useState("");
+    const  route = useRoute();
+    const sort = route.params
+  
     const Search:FC = () =>{
        return(
            <SearchBar onClick={(msg:string)=>{setNameSearch(msg)}}/>
@@ -23,7 +27,7 @@ export const Contacts:FC = (props) => {
     <MainFrame header="home" headerMenu={["Menu2",["Contacts"]]} injectHeader={<Search/>}>
     
       {
-        contacts.filter(ct => (`${ct.firstName.toUpperCase()} ${ct.lastName.toUpperCase()}`).includes(nameSearch.toUpperCase())).map((item) =>{
+        contacts.filter(ct => (`${ct.firstName.toUpperCase()} ${ct.lastName.toUpperCase()}`).includes((sort && client) ? `${client.firstName} ${client.lastName}`.toUpperCase() : nameSearch.toUpperCase())).map((item,index) =>{
           return( <ContactCard key={item.userid} contactId={item.userid} phoneNumber={item.phone} name={`${item.firstName} ${ item.lastName}`}/>)
         })
       }
