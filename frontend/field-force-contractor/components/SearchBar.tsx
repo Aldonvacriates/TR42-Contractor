ca9875fc8c5b4e118a352b086b944626275dd70b
Jonathanch2022 <admin@JCH-LPT2>
2026-05-08 12:05:18 -0700

    Refactored the search bar to dynamically float above the keyboard so user input remains visible while typing. Also renamed “previousMessages” to “messages” in the demo data, reflecting that all messages (new and previous) are part of the same unified collection.

--- frontend/field-force-contractor/components/SearchBar.tsx
+++ frontend/field-force-contractor/components/SearchBar.tsx
@@ -1,7 +1,7 @@
-import {View,TextInput,Pressable,Text,Image} from 'react-native';
-import {Styles} from '@/constants/Styles';
-import {FC, useState} from "react";
-import { Assets } from "@/constants/Assets";
+import { Styles } from '@/constants/Styles';
+import { AppContext } from '@/contexts/AppContext';
+import { FC, useContext, useEffect, useState } from "react";
+import { Keyboard, Pressable, Text, TextInput, View } from 'react-native';
 
 
 type props = {
@@ -11,14 +11,28 @@ type props = {
     multiline?: boolean
     onClick:Function
     resetOnSubmit?:Boolean
+    reactKeyboard?:Boolean
 }
+
 export const SearchBar:FC<props> = (props) => {
  const searchPlaceHolder = props.placeHolder || "Search...";
  const buttonText = props.buttonText || "Submit";
  const[message,setMessage] = useState<string>("");
  const [inputHeight,setInputHeight] = useState<number>(37);
+ const [floatHeight,setFloatHeight] = useState<number>(0);
+ const {menuHeight} = useContext(AppContext);
  const maxInputHeight = 110;
+
+ useEffect(() => {
+  if(props.reactKeyboard){
+  Keyboard.addListener("keyboardDidShow",(event) => {setFloatHeight(((event.endCoordinates.height - menuHeight) > 0) ? event.endCoordinates.height - menuHeight : 0)})
+  Keyboard.addListener("keyboardDidHide",(event) => {setFloatHeight(0)})
+  }
+
+},[])
+
     return(<>
+     
     <View style={Styles.SearchBar.Bar}>
         <TextInput
           value={message}
@@ -54,9 +68,9 @@ export const SearchBar:FC<props> = (props) => {
                   }
               }
          </Pressable>
-         
+        
     </View>
-    
+    <View style={[Styles.Chat.floatBox,{height:floatHeight}]}/>
     </>
 
     )
